e949be8c404d4e15df49626b49815f963f1907dc
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-02-18 13:33:12 +0600

    install required packages

---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { navbarLink } from "../../database/NavbarData";
+
+const Navbar = () => {
+    return <div>navbar</div>;
+};
+
+export default Navbar;
---
+++ src/database/NavbarData.js
@@ -0,0 +1,27 @@
+export const navbarLink = [
+    {
+        id: 1,
+        url: "/",
+        text: "home",
+    },
+    {
+        id: 2,
+        url: "/menu",
+        text: "menu",
+    },
+    {
+        id: 3,
+        url: "/about",
+        text: "about us",
+    },
+    {
+        id: 4,
+        url: "/contact",
+        text: "contact",
+    },
+    {
+        id: 5,
+        url: "/dashboard",
+        text: "dashboard",
+    },
+];
