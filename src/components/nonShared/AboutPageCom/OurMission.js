9432350cf3c00f6fac0cd16893d1b8ce0d287c5b
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2023-02-19 12:31:53 +0600

    add our mission and vision

---
+++ src/components/nonShared/AboutPageCom/OurMission.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const OurMission = () => {
+    return (
+        <section className="w-full max-w-7xl mx-auto px-3 xs:px-5 sm:px-8 md:px-8 xl:px-0">
+            <h2 className="text-green-100 text-lg sm:text-xl md:text-2xl uppercase font-medium tracking-wide text-center">
+                our story
+            </h2>
+            <p className="text-black opacity-90 text-sm md:text-base text-justify w-full max-w-[800px] mx-auto mt-3 md:mt-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex
+                impedit maxime quibusdam odit atque totam, quas doloremque est
+                officia exercitationem, facere dolores ab inventore minus illo,
+                molestias tenetur? Error itaque optio architecto dolor minus
+                autem.
+            </p>
+            <p className="text-black opacity-90 text-sm md:text-base text-justify w-full max-w-[800px] mx-auto mt-3 md:mt-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex
+                impedit maxime quibusdam odit atque totam, quas doloremque est
+                officia exercitationem, facere dolores ab inventore minus illo,
+                molestias tenetur? Error itaque optio architecto dolor minus
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Praesentium, beatae! autem.
+            </p>
+            <p className="text-black opacity-90 text-sm md:text-base text-justify w-full max-w-[800px] mx-auto mt-3 md:mt-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex
+                impedit maxime quibusdam odit atque totam, quas doloremque est
+                autem.
+            </p>
+        </section>
+    );
+};
+
+export default OurMission;
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AboutHero from "../components/nonShared/AboutPageCom/AboutHero";
+import OurMission from "../components/nonShared/AboutPageCom/OurMission";
+import OurVision from "../components/nonShared/AboutPageCom/OurVision";
 
 const AboutPage = () => {
     return (
         <div>
             <AboutHero />
+            <OurMission />
+            <OurVision />
         </div>
     );
 };
